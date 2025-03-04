083b2dd456f049d9ed107669a06cd57946544e59
mohamed-belall <mohamedbelal.eng@gmail.com>
2025-03-04 03:36:06 +0200

    add D5_Part 07 Unit Of Work

---
+++ Talabat.Belal.Solution/Talabat.Core/IUnitOfWork.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+using Talabat.Core.Entities.Order_Aggregate;
+using Talabat.Core.Repositories.Contract;
+
+namespace Talabat.Core
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+
+
+
+        IGenericRepository<TEntity> Repository<TEntity>() where TEntity : BaseEntity;
+
+
+        Task<int> CompleteAsync();
+
+        // we can have more than one unit of work
+        //      if we have more than one repository layer
+        //          if we have more than one DbContext
+
+
+
+
+
+
+
+        // create property signature for every and each repository
+        //public IGenericRepository<Product> ProductsRepo { get; set; }
+        //public IGenericRepository<ProductBrand> ProductBrandsRepo { get; set; }
+        //public IGenericRepository<ProductCategory> ProductCategorysRepo { get; set; }
+        //public IGenericRepository<DeliveryMethod> DeliveryMethodsRepo { get; set; }
+        //public IGenericRepository<OrderItem> OrderItemsRepo { get; set; }
+        //public IGenericRepository<Order> OrdersRepo { get; set; }
+
+    }
+}
---
+++ Talabat.Belal.Solution/Talabat.Repository/UnitOfWork.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core;
+using Talabat.Core.Entities;
+using Talabat.Core.Entities.Order_Aggregate;
+using Talabat.Core.Repositories.Contract;
+using Talabat.Repository.Data;
+
+namespace Talabat.Repository
+{
+        // unit of work call DbContext that call Database
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly StoreContext _storeContext;
+
+        //private Dictionary<string, GenericRepository<BaseEntity>> _repositories; 
+        private Hashtable _repositories; 
+
+
+        public UnitOfWork(StoreContext storeContext) // ask CLR for creating object from DbContext Implecitly
+        {
+            this._storeContext = storeContext;
+            //_repositories = new Dictionary<string, GenericRepository<BaseEntity>>()
+            _repositories = new Hashtable();
+        }
+
+        public IGenericRepository<TEntity> Repository<TEntity>() where TEntity : BaseEntity
+        {
+            #region using Dictionary
+            //var key = typeof(TEntity).Name;
+            //if (!_repositories.ContainsKey(key))
+            //{
+            //    var repository = new GenericRepository<TEntity>(_storeContext) as GenericRepository<BaseEntity>;
+            //    _repositories.Add(key, repository);
+            //}
+
+            //return _repositories[key] as IGenericRepository<TEntity>;
+            #endregion
+
+            #region using HashTable
+            var key = typeof(TEntity).Name;
+            if (!_repositories.ContainsKey(key))
+            {
+                var repository = new GenericRepository<TEntity>(_storeContext) ;
+                _repositories.Add(key, repository);
+            }
+
+            return _repositories[key] as IGenericRepository<TEntity>; 
+            #endregion
+        }
+
+        public Task<int> CompleteAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        // automatic property 
+        // compiler will generate backing field => hidden parameter attribute 
+        // for each property => create get and set
+        //public IGenericRepository<Product> ProductsRepo {get; set;}
+        //public IGenericRepository<ProductBrand> ProductBrandsRepo {get; set;}
+        //public IGenericRepository<ProductCategory> ProductCategorysRepo {get; set;}
+        //public IGenericRepository<DeliveryMethod> DeliveryMethodsRepo {get; set;}
+        //public IGenericRepository<OrderItem> OrderItemsRepo {get; set;}
+        //public IGenericRepository<Order> OrdersRepo {get; set;}
+
+    }
+}
